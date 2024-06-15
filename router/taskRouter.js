09e709f5a0cbe0dadfdc573193b42de7503bdd21
Himanshu Raj <himanshuraj6771@gmail.com>
2024-06-16 05:07:38 +0530

    Bug fixed and getTask and getAllTask completed

--- router/taskRouter.js
+++ router/taskRouter.js
@@ -2,10 +2,14 @@ const taskRouter = require('express').Router();
 const createTask = require('../controller/createTaskController');
 const removeTask = require('../controller/removeTaskController');
 const updateTask = require('../controller/updateTask_controller');
+const getTask = require('../controller/getTaskController');
+const getAllTask = require('../controller/getAllTaskController');
 
 taskRouter
  .post('/', createTask)
  .delete('/', removeTask)
  .put('/:id', updateTask)
+ .get('/:id', getTask)
+ .get('/', getAllTask)
 
 module.exports = taskRouter;
